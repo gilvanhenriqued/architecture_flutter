d4b350722714e7316aa501876d1427f41dd5825a
Gilvan Henrique <gilvanh.15@gmail.com>
2020-07-12 06:37:38 -0300

    using ing shared local storage service  to keep the theme

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,19 +1,27 @@
+import 'package:architecture_flutter/app/interfaces/local_storage_interface.dart';
 import 'package:architecture_flutter/app/models/appconfig_model.dart';
+import 'package:architecture_flutter/app/services/shared_local_storage_service.dart';
 import 'package:flutter/widgets.dart';
 
 class AppController {
 
   static final AppController instance = AppController._();
-
-  AppController._();
+  AppController._(){
+    storage.get('isDark').then((value){
+      if(value != null) config.themeSwitch.value = value;
+    });
+  }
 
   final AppConfigModel config = AppConfigModel();
 
   bool get isDark => config.themeSwitch.value;
   ValueNotifier<bool> get themeSwitch => config.themeSwitch;
+
+  final ILocalStorage storage = SharedLocalStorageService();
   
   changeTheme(bool value) {
     config.themeSwitch.value = value;
+    storage.put('isDark', value);
   }
 
 }
---
+++ lib/app/services/shared_local_storage_service.dart
@@ -0,0 +1,31 @@
+import 'package:architecture_flutter/app/interfaces/local_storage_interface.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedLocalStorageService implements ILocalStorage {
+  @override
+  Future get(String key) async {
+    var shared = await SharedPreferences.getInstance();
+    return shared.get(key);
+  }
+
+  @override
+  Future put(String key, value) async {
+    var shared = await SharedPreferences.getInstance();
+
+    if(value is bool) {
+      shared.setBool(key, value);
+    } else if (value is String) {
+      shared.setString(key, value);
+    } else if (value is int) {
+      shared.setInt(key, value);
+    } else if (value is double) {
+      shared.setDouble(key, value);
+    }
+  }
+
+  @override
+  Future delete(String key) async {
+    var shared = await SharedPreferences.getInstance();
+    return shared.remove(key);
+  }
+}
